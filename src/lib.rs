02cee939f39ecbfcc819496942f4bcc8d3f7f990
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-07-20 23:01:36 +0100

    Fix and simplify logic; add initial tests using UnixStreams

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,9 @@ mod blocking;
 #[derive(Debug)]
 pub struct LineReaderNonBlock<R: AsRawFd + Read> {
     reader: R,
-    eof: bool,
+    eof_: bool,
     buf: Vec<u8>,
+    used: usize,
     lines: Vec<String>,
 }
 
@@ -22,29 +23,35 @@ impl<R: AsRawFd + Read> LineReaderNonBlock<R> {
         blocking::disable(fd)?;
         Ok(Self {
             reader,
-            eof: false,
+            eof_: false,
             buf: Default::default(),
+            used: 0,
             lines: Default::default(),
         })
     }
 
+    pub fn eof(&self) -> bool {
+        self.eof_
+    }
+
+    fn u8array_to_string(buf: &[u8]) -> Result<String, io::Error> {
+        match str::from_utf8(buf) {
+            Ok(line) => Ok(line.to_string()),
+            Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+        }
+    }
+
     fn eval_buf(&mut self, mut pos: usize) -> Result<(), io::Error> {
         loop {
-            if let Some(inewline) = memchr::memchr(b'\n', &self.buf[pos..]) {
+            if let Some(inewline) = memchr::memchr(b'\n', &self.buf[pos..self.used]) {
                 // Found a newline.
-                let mut line = self.buf.split_off(pos + inewline);
+                let mut line = self.buf.split_off(pos + inewline + 1);
+                self.used -= pos + inewline + 1;
                 // They are swapped at the moment, unswap:
                 mem::swap(&mut self.buf, &mut line);
                 // Convert line to string and append to self.lines:
-                match str::from_utf8(&line) {
-                    Ok(line) => {
-                        self.lines.push(line.to_string());
-                        pos = 0;
-                    }
-                    Err(e) => {
-                        return Err(io::Error::new(io::ErrorKind::InvalidData, e));
-                    }
-                }
+                self.lines.push(Self::u8array_to_string(&line)?);
+                pos = 0;
             } else {
                 // No newline read.
                 return Ok(());
@@ -53,17 +60,24 @@ impl<R: AsRawFd + Read> LineReaderNonBlock<R> {
     }
 
     pub fn read_once(&mut self) -> Result<bool, io::Error> {
-        if self.eof {
+        if self.eof_ {
             return Ok(false);
         }
-        let oldlen = self.buf.len();
-        self.buf.reserve(1024);
+        if self.buf.len() < self.used + 1024 {
+            self.buf.resize(self.used + 1024, 0);
+        }
+        let oldused = self.used;
         let buf = self.buf.as_mut_slice();
-        match self.reader.read(&mut buf[oldlen..]) {
+        let r = self.reader.read(&mut buf[self.used..]);
+        match r {
             Ok(0) => {
-                self.buf.push(b'\n');
-                self.eval_buf(0)?;
-                self.eof = true;
+                if self.used > 0 {
+                    let mut lastline = mem::take(&mut self.buf);
+                    lastline.truncate(self.used);
+                    self.lines.push(Self::u8array_to_string(&lastline)?);
+                    self.used = 0;
+                }
+                self.eof_ = true;
             }
             Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
                 // No data availble, just let the function return
@@ -71,8 +85,10 @@ impl<R: AsRawFd + Read> LineReaderNonBlock<R> {
             Err(ref err) if err.kind() == io::ErrorKind::Interrupted => {
                 // Interrupted, just let the function return
             }
-            Ok(_len) => {
-                self.eval_buf(oldlen)?;
+            Ok(len) => {
+                self.used += len;
+                // Look for newlines from "oldused" forward:
+                self.eval_buf(oldused)?;
             }
             Err(err) => {
                 return Err(err);
